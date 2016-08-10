51150ac2ab52f5c0943a6ee839ae2afa053e7d3d
Steven Maude <git@stevenmaude.co.uk>
2016-08-10 23:10:05 +0100

    Add initial working code
    Functional, and seems to give the same result as postrunner does.
    However, I can't get some of the additional checks that postrunner makes
    to succeed here, so they're currently left out. (For example, that
    checks if the three bytes between the 2307 are zero, but I can't get
    that check to pass, so maybe I'm doing something wrong...)

---
+++ armstrong.go
@@ -0,0 +1,81 @@
+// Based on https://www.kluenter.de/garmin-ephemeris-files-and-linux/ and
+// EPO_Downloader.rb in https://github.com/scrapper/postrunner (GPLv2)
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+)
+
+// retrieveData makes a HTTP request to get Garmin EPO data and returns the body as []byte if successful.
+func retrieveData() ([]byte, error) {
+	url := "https://omt.garmin.com/Rce/ProtobufApi/EphemerisService/GetEphemerisData"
+	// Data from https://www.kluenter.de/garmin-ephemeris-files-and-linux/
+	data := []byte("\n-\n\aexpress\u0012\u0005de_DE\u001A\aWindows\"" +
+		"\u0012601 Service Pack 1\u0012\n\b\x8C\xB4\x93\xB8" +
+		"\u000E\u0012\u0000\u0018\u0000\u0018\u001C\"\u0000")
+
+	resp, err := http.Post(url, "application/octet-stream", bytes.NewBuffer(data))
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	return body, nil
+}
+
+// checkDataLength checks the EPO data length; if not as expected, returns an error.
+func checkDataLength(data []byte) error {
+	dataLength := len(data)
+	// Each EPO data set is 2307 bytes long, with the first three bytes to be removed.
+	if dataLength != 28*2307 {
+		return fmt.Errorf("EPO data has unexpected length: %v", dataLength)
+	}
+	return nil
+}
+
+// cleanEPO removes the first three bytes from each block of 2307 bytes in EPO data,
+// and returns a cleaned []byte.
+func cleanEPO(rawEPOData []byte) []byte {
+	var outData []byte
+	for i := 0; i <= 27; i++ {
+		offset := i * 2307
+		outData = append(outData, rawEPOData[offset+3:offset+2307]...)
+	}
+	return outData
+}
+
+// main retrieves EPO data, checks it, cleans it and writes it to disk.
+func main() {
+	fmt.Println("Retrieving data from Garmin's servers...")
+	rawEPOData, err := retrieveData()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Processing EPO.BIN...")
+	err = checkDataLength(rawEPOData)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = ioutil.WriteFile("raw", rawEPOData, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	outData := cleanEPO(rawEPOData)
+
+	err = ioutil.WriteFile("EPO.BIN", outData, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Done! EPO.BIN saved.")
+}
